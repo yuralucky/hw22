1b91f8b08bd3470935dff0e6a492e2652a641734
yuriy <yuralucky83@gmail.com>
2021-05-08 15:16:56 +0300

    first commit

---
+++ app/Service/UserAgent/JenssegersUserAgent.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Service\UserAgent;
+
+
+class JenssegersUserAgent implements UserAgent
+{
+
+    public function browser()
+    {
+        // TODO: Implement browser() method.
+    }
+
+    public function system()
+    {
+        // TODO: Implement system() method.
+    }
+}
---
+++ app/Service/UserAgent/PlainUserAgent.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Service\UserAgent;
+
+
+class PlainUserAgent implements UserAgent
+{
+
+    public function browser()
+    {
+        // TODO: Implement browser() method.
+    }
+
+    public function system()
+    {
+        // TODO: Implement system() method.
+    }
+}
